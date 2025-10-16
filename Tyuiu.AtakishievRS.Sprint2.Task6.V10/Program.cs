b59c97ff31ae523867912a825fd1a2193e04428a
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-10-17 00:23:37 +0500

    Task6

---
+++ Tyuiu.AtakishievRS.Sprint2.Task6.V10/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.AtakishievRS.Sprint2.Task6.V10.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Получение результата из switch                                    *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+        Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите номер дня: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите номер месяца: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите номер года: ");
+        int g = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Дата предыдущего дня: " + ds.FindDateOfPreviousDay(g, m, n));
+
+        Console.ReadKey();
+    }
+}
