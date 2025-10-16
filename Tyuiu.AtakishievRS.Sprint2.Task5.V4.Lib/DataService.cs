565a21eb488efcf3480e84d2a06996185fc20134
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-10-16 15:37:14 +0500

    Task5

---
+++ Tyuiu.AtakishievRS.Sprint2.Task5.V4.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AtakishievRS.Sprint2.Task5.V4.Lib
+{
+    public class DataService : ISprint2Task5V4
+    {
+        public string FindCardSuit(int value)
+        {
+            string card;
+            switch (value)
+            {
+                case 1:
+                    card = "пики";
+                    break;
+                case 2:
+                    card = "трефы";
+                    break;
+                case 3:
+                    card = "бубны";
+                    break;
+                case 4:
+                    card = "червы";
+                    break;
+                default:
+                    card = "Не верный номер масти";
+                    break;
+            }
+            return card;
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint2.Task5.V4/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.AtakishievRS.Sprint2.Task5.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #4                                                              *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая использует оператор switch                  *");
+        Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        int x;
+        Console.WriteLine("Введите номер масти:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine("Масть карты - " + ds.FindCardSuit(x));
+        Console.ReadKey();
+    }
+}
