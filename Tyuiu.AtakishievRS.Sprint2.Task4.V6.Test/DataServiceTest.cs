9cc7e12d2d28632c8594f0cc700b8a32e5f9169a
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-10-16 14:46:20 +0500

    Task4

---
+++ Tyuiu.AtakishievRS.Sprint2.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AtakishievRS.Sprint2.Task4.V6.Lib;
+namespace Tyuiu.AtakishievRS.Sprint2.Task4.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(1, 1);
+
+            Assert.AreEqual(0.667, res, 0.001);
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint2.Task4.V6/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.AtakishievRS.Sprint2.Task4.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Тернарный оператор                                                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу на, которая вычисляет выражения с помощью            *");
+        Console.WriteLine("* тернарного оператора, где пользователь вводит значения x и y            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение X:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение y:");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        var res = ds.Calculate(x, y);
+        Console.WriteLine(Math.Round(res, 3));
+        Console.ReadKey();
+    }
+}
